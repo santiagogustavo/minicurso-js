f913618504873018fff9f23e05ac9f90f0da73e1
santiagogustavo <gs4n.96@gmail.com>
2018-09-14 15:07:46 -0300

    :clipbook: (+) Adiciona tópico "Origens"

--- src/views/WtfIsJs/WtfIsJs.jsx
+++ src/views/WtfIsJs/WtfIsJs.jsx
@@ -3,7 +3,10 @@ import {
   H1,
   H3,
   Content,
+  CodeArea,
+  CodeComment,
   InfoText,
+  CentralizedImg,
 } from 'components/styles';
 import Accordion from 'components/Accordion';
 import { NavLinks, NextLink } from 'components/Links';
@@ -17,9 +20,78 @@ const WtfIsJs = () => (
         a linguagem.
       </InfoText>
       <Accordion title="1.1. Origem">
-        1.1. Origem
-        <H3>Protótipo</H3>
-        <H3>Não é Java!</H3>
+        <CodeArea>
+          <CodeComment style={{ paddingBottom: 0 }}>
+            <b>Javascript</b>, ou simplesmente <b>JS</b>, é uma linguagem de programação de&nbsp;
+            <a href="https://becode.com.br/linguagens-alto-nivel-x-baixo-nivel/">alto nível</a> desenvolvida em meados
+            de 1996 pelo programador <strike>Tom Hanks</strike> <b>Brendan Eich</b>, que foi posteriormente um dos
+            fundadores da <b>Mozilla Corporation</b>. A linguagem foi desenvolvida no auge da <b>Netscape</b>, como uma
+            solução para rodar <b>scripts do lado do cliente</b> e <b>interagir a interações de usuário</b>, eliminando
+            a necessidade de pedir requisições para um servidor.
+            <br /><br />
+            Inicialmente desenvolvida para o <b>Netscape Navigator</b>, teve seu suporte extendido para outras
+            plataformas e atualmente é a <b>linguagem mais popular para programação client-side.</b> Assim como o&nbsp;
+            <b>
+              <a href="https://www.hostinger.com.br/tutoriais/o-que-e-html-conceitos-basicos/">HTML</a> e o&nbsp;
+              <a href="https://www.hostinger.com.br/tutoriais/o-que-e-css-guia-basico-de-css/">CSS</a>
+            </b>, passou por mudanças cruciais e hoje é utilizada também para&nbsp;
+            <b>códigos do lado do servidor</b>.
+          </CodeComment>
+          <CodeComment style={{ display: 'flex', alignItems: 'center' }}>
+            <CentralizedImg
+              alt="brendan_eich"
+              // eslint-disable-next-line
+              src="https://cnet4.cbsistatic.com/img/gRItWJMKsaqeAKIOnSbD31qzwew=/570x0/2012/02/26/24cf0440-f0e6-11e2-8c7c-d4ae52e62bcc/20120226_Brendan_Eich_001.jpg"
+              style={{
+                height: 200,
+              }}
+            />
+          </CodeComment>
+        </CodeArea>
+        <CodeArea>
+          <CodeComment style={{ paddingBottom: 0, paddingTop: 0 }}>
+            <H3>Protótipo</H3>
+            Para defender a <b>proposta</b> da linguagem em relação com outras já existentes e estreitar as relações
+            com a <b>Sun Microsystems (desenvolvedora do Java)</b>, a <b>Netscape</b> encarregou <b>Eich</b> para
+            elaborar um protótipo.
+            <br /><br />
+            O protótipo, originalmente chamado de <b>Mocha</b> e lançado como <b>LiveScript</b> na sua versão Beta,
+            foi feito em <b>apenas 10 dias!</b>
+          </CodeComment>
+          <CodeComment style={{ display: 'flex', alignItems: 'center' }}>
+            <CentralizedImg
+              alt="netscape"
+              // eslint-disable-next-line
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Netscape_logo.svg/2000px-Netscape_logo.svg.png"
+              style={{
+                borderRadius: 0,
+                height: 50,
+              }}
+            />
+          </CodeComment>
+        </CodeArea>
+        <CodeArea>
+          <CodeComment style={{ paddingTop: 0 }}>
+            <H3>Não é Java!</H3>
+            Seu nome pode sugerir uma similaridade a linguagem&nbsp;
+            <a href="https://www.java.com/en/download/faq/whatis_java.xml">Java</a>, mas não se engane:&nbsp;
+            <b>Javascript não tem nada a ver com Java!</b> De fato a confusão vem desde seu lançamento, já que a&nbsp;
+            <b>Netscape</b> também desenvolvia na época o suporte para <b>Java</b>.
+            <br /><br />
+            Seu nome final foi o resultado de uma <b>campanha de marketing</b> em conjunto com a&nbsp;
+            <b>Sun Microsystems</b> para aproveitar a popularidade das linguagens (e lançá-las em conjunto).
+          </CodeComment>
+          <CodeComment style={{ display: 'flex', alignItems: 'center' }}>
+            <CentralizedImg
+              alt="java"
+              src="https://diylogodesigns.com/wp-content/uploads/2017/07/java-logo-vector-768x768.png"
+              style={{
+                borderRadius: 0,
+                height: 150,
+              }}
+            />
+          </CodeComment>
+        </CodeArea>
       </Accordion>
       <Accordion title="1.2. Evolução">
         1.2. Evolução
